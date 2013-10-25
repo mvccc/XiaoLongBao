6e98e1b1f44cc8feb6f5848114ae0ba0a7e42718
morningwinter <morningwinter@my-mulu.com>
2013-10-24 21:55:46 -0700

    MVCCC header, footer and menu

---
+++ application/views/templates/footer.php
@@ -0,0 +1,13 @@
+
+      	<footer>
+        	<p>&copy; MVCCC 2013</p>
+     	</footer>
+
+    </div><!--/.container-->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./assets/js/jquery.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,93 @@
+
+<!DOCTYPE html>
+<html lang="zh">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="mvccc,church, mountain view, 山景城中國基督教會, 教會">
+
+    <title>MVCCC</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="mvccc.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">MVCCC</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#">教會首頁</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">教會介紹<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">教會簡介</a></li>
+                <li><a href="#">信仰宣言</a></li>
+                <li><a href="#">教會章程</a></li>
+                <li><a href="#">牧者介紹</a></li>
+                <li><a href="#">服侍部門</a></li>
+                <li><a href="#">地址電話</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">神家近況<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">最新消息</a></li>
+                <li><a href="#">近期活動</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">主日崇拜<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">主日信息</a></li>
+                <li><a href="#">聚會時間</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">教會生活<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">團契生活</a></li>
+                <li><a href="#">主日學</a></li>
+                <li><a href="#">詩班</a></li>
+                <li><a href="#">信仰討論</a></li>
+                <li><a href="#">禱告會</a></li>
+                <li><a href="#">預查</a></li>
+                <li><a href="#">AWANA</a></li>
+              </ul>
+            </li>
+            <li><a href="#">代禱贊美</a></li>
+            <li><a href="#">差傳事工</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">資源中心<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">重要鏈接</a></li>
+                <li><a href="#">照片集錦</a></li>
+                <li><a href="#">錄音錄像</a></li>
+              </ul>
+            </li>
+            <li><a href="#">English</a></li>
+          </ul>
+        </div><!-- /.nav-collapse -->
+      </div><!-- /.container -->
+    </div><!-- /.navbar -->
+
+    <div class="container">
