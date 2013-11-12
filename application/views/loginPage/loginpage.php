1169932414862c898ec0c8400534154ebd8a30a3
Haiqing Jiang <hqjiang1988@gmail.com>
2013-11-11 22:52:11 -0800

    add sign up page

--- application/views/loginPage/loginpage.php
+++ application/views/loginPage/loginpage.php
@@ -1,29 +1,3 @@
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta name="keywords" content="mvccc,church, mountain view, 山景城中國基督教會, 教會">
-
-    <title>MVCCC Login</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="<?php echo base_url(); ?>assets/css/signin.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
   <body>
     <div class="container">
       <form class="form-signin">
@@ -34,8 +8,7 @@
           <input type="checkbox" value="remember-me"> 記住我
         </label>
         <button class="btn btn-lg btn-primary btn-block" type="submit">登錄</button>
+        <button class="btn-block"><a href="<?php echo base_url(); ?>index.php/pages/login/signuppage">註冊帳號</a></button>
       </form>
     </div> <!-- /container -->
   </body>
-</html>
-
