e8aa8d124379f0018833853c4d606873a26c7555
Haiqing Jiang <hqjiang1988@gmail.com>
2013-11-17 01:04:10 -0800

    fix bugs of buttons in signuppage and add one line instruction in loginpage

--- application/views/loginPage/signuppage.php
+++ application/views/loginPage/signuppage.php
@@ -3,9 +3,9 @@
             <input type="text" class="form-control" placeholder="名字" required autofocus>
             <input type="text" class="form-control" placeholder="姓氏" required autofocus>
             <input type="text" class="form-control" placeholder="郵件" required autofocus>
-            <p><div name="gender" class="btn-group" data-toggle="buttons-radio">
-              <button type="button" class="btn btn-info">男</button>
-              <button type="button" class="btn btn-info">女</button>
+            <p><div>
+              <button type="button" class="btn btn-default">男</button>
+              <button type="button" class="btn btn-default">女</button>
             </div></p>
             <input type="password" class="form-control" placeholder="密碼" required autofocus>
             <input type="password" class="form-control" placeholder="確認密碼" required autofocus>
