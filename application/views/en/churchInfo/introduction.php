a3e83f36f8127231cd0993199242dc624d42d43c
morningwinter <morningwinter@my-mulu.com>
2013-11-19 23:39:30 -0800

    Church introduction (English)

--- application/views/en/churchInfo/introduction.php
+++ application/views/en/churchInfo/introduction.php
@@ -1,5 +1,27 @@
-<div class="row">
-  <div class="col-lg-12 well">
-    <h2>Our Church Page</h2>
+<div class="row well">
+  <div class="col-lg-12">
+    <h2>Welcome to Intersect at MVCCC!</h2>
+    <p><i>
+    We are the English Congregation of Mountain View Chinese Christian Church (MVCCC) which is an independent, evangelical, non-denominational church.
+    A predominantly second generation Asian American congregation, our members include jr. high and high school students, young adults (college and 
+    working adults), young families (parents with infants and toddlers), and older fmilies (parents of our college-aged students).
+	</i></p>
+	<p><i>
+	We absolutely love our intergenerational Church Family and invite you to visit us!
+	</i></p>
+	<br/>
+	<h2> Mountain View Chinese Christian Church</h2>
+	<p><i>
+	MVCCC is comprised of two different congregations. <strong>The English Congregation</strong> is a mix of students, single adults, and families. We are proud of the fact that we are an inter-generational church and
+	we embrace this unique identity. <strong>The Chinese Congregation</strong> also meets on Sunday mornings (11am) and offers a Sunday Service, a number of fellowsip, small groups, bible study groups,
+	and children programs in Mandarin Chinese.
+	</i></p>
+	</br>
+	<h2>Our History</h2>
+	<p><i>
+	Started in the 1960s as a Home Bible Study Group under the guidance and assistance of Rev. Moses Yu, Chinese Christian Fellowship held its first
+	worship service in 1964 in the hall rented from Masonic Temple. In 1974, it formally changed its name to Mountain View Chinese Christian Church,
+	and on December 8, 1996, MVCCC moved into its current facility.
+	</i></p>
   </div>
 </div>
