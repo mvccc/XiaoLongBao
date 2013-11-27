4f664647e192d4671b860d0b4d3bd6f24a8d53a6
morningwinter <morningwinter@my-mulu.com>
2013-11-27 01:10:47 -0800

    Recent Events on Home Page

--- application/views/ch/index.php
+++ application/views/ch/index.php
@@ -1,4 +1,9 @@
-      <div class="row well">
+<div class="row well">
+  <br>
+  <div class="col-lg-12">
+
+      <!-- first row -->
+      <div class="row">
         <div class="col-lg-8">
          <div id="carousel-generic" class="carousel slide">
             <!-- Indicators -->
@@ -11,19 +16,19 @@
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
               <div class="item active">
-                  <img data-src="holder.js/900x400/auto/#777:#ffffff/text:First slide" alt="First slide">
+                  <img data-src="holder.js/800x400/auto/gray:#000/text:First slide" alt="First slide">
                    <div class="carousel-caption">
                       First Slide
                   </div>
               </div>
               <div class="item">
-                 <img data-src="holder.js/900x400/auto/#777:#ffffff/text:Second slide" alt="second slide">
+                 <img data-src="holder.js/800x400/auto/gray:#000/text:Second slide" alt="second slide">
                    <div class="carousel-caption">
                       Second Slide
                   </div>
               </div>
               <div class="item">
-                 <img data-src="holder.js/900x400/auto/#777:#ffffff/text:Third slide" alt="third slide">
+                 <img data-src="holder.js/800x400/auto/gray:#000/text:Third slide" alt="third slide">
                    <div class="carousel-caption">
                       Third Slide
                   </div>
@@ -40,9 +45,50 @@
           </div>
         </div>
         <div class="col-lg-4">
-		  <h2><span class="glyphicon glyphicon-globe"></span>&nbsp;每日金句</h2>
-          <p>神愛世人、甚至將他的獨生子賜給他們、叫一切信他的、不至滅亡、反得永生。</p>
-          <small><cite class="pull-right">約翰福音 3:16</cite></small>
+          <div class="panel panel-default mvccc-panel">
+            <div class="panel-heading">
+              <h3 class="panel-title"><span class="glyphicon glyphicon-thumbs-up"></span>&nbsp;近期活動</h3>
+            </div>
+            <div class="panel-body">
+
+              <div class="listing">
+                <div class="event-left-listing">
+                  <div class="date">
+                    <h4>23</h4>
+                  </div>
+                </div>
+                <div class="event-right-listing">
+                  <a href="#">感恩見証會</a>
+                  <p>7:00 pm - 9:00 pm</p>
+                </div>
+              </div>
+
+              <div class="listing">
+                <div class="event-left-listing">
+                  <div class="date">
+                    <h4>25</h4>
+                  </div>
+                </div>
+                <div class="event-right-listing">
+                  <a href="#">感恩節洗禮</a>
+                  <p>11:00 am - 12:00 am</p>
+                </div>
+              </div>
+
+              <div class="listing listing-last">
+                <div class="event-left-listing">
+                  <div class="date">
+                    <h4>30</h4>
+                  </div>
+                </div>
+                <div class="event-right-listing">
+                  <a href="#">音樂見証會</a>
+                  <p>6:00 pm - 9:00 pm</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
         	<!--
         	<div class row>
           	  <img data-src="holder.js/350x166/auto/#777:#ffffff/text:Audio" alt="audio">
@@ -53,29 +99,55 @@
           	-->
         </div>    
       </div>	
-      <div class="spaceBanner"/>
-      <div class="row">
-      	<div class="col-lg-6">
-      		<img src="<?php echo base_url(); ?>assets/img/divider_1.gif"/>
-      	</div>
-       	<div class="col-lg-6">
-      		<img src="<?php echo base_url(); ?>assets/img/divider_1.gif"/>
-      	</div>
-      </div>
+      <br>
+      <hr>
+      <br>
       <div class="row">
         <div class="col-lg-4">
-          <h2><span class="glyphicon glyphicon-home"></span>&nbsp;初次來訪</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-primary" href="#" role="button">詳細內容 &raquo;</a></p>
+          <div class="panel panel-default mvccc-panel">
+            <div class="panel-heading">
+              <h3 class="panel-title"><span class="glyphicon glyphicon-home"></span>&nbsp;初次來訪</h3>
+            </div>
+            <div class="panel-body">
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-primary" href="#" role="button">詳細內容 &raquo;</a></p>
+            </div>
+          </div>
         </div>
         <div class="col-lg-4">
-          <h2><span class="glyphicon glyphicon-thumbs-up"></span>&nbsp;近期活動</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-primary" href="#" role="button">詳細內容 &raquo;</a></p>
-       </div>
+          <div class="panel panel-default mvccc-panel">
+            <div class="panel-heading">
+              <h3 class="panel-title"><span class="glyphicon glyphicon-globe"></span>&nbsp;最新消息</h3>
+            </div>
+            <div class="panel-body">
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              </p>
+              <p><a class="btn btn-primary" href="#" role="button">詳細內容 &raquo;</a></p>
+            </div>
+          </div>          
+          <!--
+          <div class="panel panel-default mvccc-panel">
+            <div class="panel-heading">
+              <h3 class="panel-title"><span class="glyphicon glyphicon-globe"></span>&nbsp;每周金句</h3>
+            </div>
+            <div class="panel-body">
+              <p>神愛世人、甚至將他的獨生子賜給他們、叫一切信他的、不至滅亡、反得永生。</p>
+              <small><cite class="pull-right">約翰福音 3:16</cite></small>
+            </div>
+          </div>
+          -->
+        </div>
         <div class="col-lg-4">
-          <h2><span class="glyphicon glyphicon-volume-up"></span>&nbsp;最新主日信息</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-          <p><a class="btn btn-primary" href="#" role="button">詳細內容 &raquo;</a></p>
+          <div class="panel panel-default mvccc-panel">
+            <div class="panel-heading">
+              <h3 class="panel-title"><span class="glyphicon glyphicon-volume-up"></span>&nbsp;最新主日信息</h3>
+            </div>
+            <div class="panel-body">
+              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+              <p><a class="btn btn-primary" href="#" role="button">詳細內容 &raquo;</a></p>
+            </div>
+          </div>
         </div>
       </div>
+  </div>
+</div>
