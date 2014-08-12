58e8d4a098e03a9f91072e2d4334d553b464fc9b
morningwinter <morningwinter@my-mulu.com>
2014-08-11 20:58:35 -0700

    Move jquery and boostrap js from footer to header

--- application/views/templates/header_en.php
+++ application/views/templates/header_en.php
@@ -22,6 +22,9 @@
       <script src="../../assets/js/html5shiv.js"></script>
       <script src="../../assets/js/respond.min.js"></script>
     <![endif]-->
+
+    <script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
   </head>
 
   <body>
