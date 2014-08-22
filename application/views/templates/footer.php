e7148ecac5c89badc6277bbb8bf5b30a98580fbc
morningwinter <morningwinter@my-mulu.com>
2014-08-21 23:55:11 -0700

    Add Foorter

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,13 +1,44 @@
-    <div class="container">    
-        <hr>
-      	<footer>
-        	<p>&copy; MVCCC 2013</p>
-     	</footer>
-    </div>
+
+    <footer class="mvccc-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <h3>重要鏈接</h3>
+                    <b>主日崇拜</b><br>
+                    <b>日歷活動</b><br>
+                    <b>AWANA</b><br>
+                    
+                </div>
+                <div class="col-lg-3">
+                    <h3>資源中心</h3>
+                    <b>信仰探討</b><br>
+                    <b>團契生活</b><br>
+                    <b>照片集錦</b><br>
+                </div>
+                <div class="col-lg-3">
+                    <h3>其他鏈接</h3>
+                    <b>免責聲明</b><br>
+                    <b>隱私政策</b><br>
+                    <b>Credits</b><br>
+                </div>
+                <div class="col-lg-3">
+                    <h3>聯係我們</h3>
+                        <strong>會址﹕山景城中國基督教會</strong><br>
+                        Mountain View Chinese Christian Church<br>
+                        175 East Dana Street<br>
+                        Mountain View, CA 94041<br>
+                        <span>電話</span> 650-964-1591<br>
+                        <span>傳真</span> 650-964-8491<br>
+                </div>
+            </div>
+    	   <p>&copy; MVCCC 2014</p>
+        </div>
+    </footer>
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="<?php echo base_url(); ?>assets/js/jquery.slimscroll.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.stellar.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/mvccc.js"></script>
 
     <?php
